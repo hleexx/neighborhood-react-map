87de2331e88973e1579368283b65f3af0e877e59
Hellen Lee <hleexx@gmail.com>
2018-10-08 20:13:41 -0500

    refactor: Move places variable to locations file

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,35 +1,7 @@
 import React, {Component} from 'react';
-import GoogleMapReact from 'google-map-react'
-import Place from './Place'
-
-const places = [
-	{
-		name: "O Ya",
-		lat: 42.351859,
-		lng: -71.056815
-	},
-	{
-		name: "Gene's Flatbread",
-		lat: 42.353671,
-		lng: -71.059603
-	},
-	{
-		name: "Row 34",
-		lat: 42.349872,
-		lng: -71.047243
-	},
-	{
-		name: "Lolita",
-		lat: 42.350611,
-		lng: -71.051165
-	},
-	{
-		name: "Flour Bakery",
-		lat: 42.351556,
-		lng: -71.048676
-	},	
-
-]
+import GoogleMapReact from 'google-map-react';
+import Place from './Place';
+import locations from '../data/locations';
 
 class Map extends Component {
     render() {
@@ -39,7 +11,7 @@ class Map extends Component {
 	          		bootstrapURLKeys={{ key: "AIzaSyB_TOMwP-bLmM-Fz4rfd-BhBZTNa4Y6S-c" }}
 	          		center={this.props.center}
 	          		zoom={this.props.zoom}>
-	   				{places.map(place => <Place name={place.name} lat={place.lat} lng={place.lng}/>)}
+	   				{locations.map(location => <Place name={location.name} lat={location.lat} lng={location.lng}/>)}
 	        	</GoogleMapReact>
 	        </div>
         )
---
+++ src/data/locations.js
@@ -0,0 +1,31 @@
+
+const locations = [
+	{
+		name: "O Ya",
+		lat: 42.351859,
+		lng: -71.056815
+	},
+	{
+		name: "Gene's Flatbread",
+		lat: 42.353671,
+		lng: -71.059603
+	},
+	{
+		name: "Row 34",
+		lat: 42.349872,
+		lng: -71.047243
+	},
+	{
+		name: "Lolita",
+		lat: 42.350611,
+		lng: -71.051165
+	},
+	{
+		name: "Flour Bakery",
+		lat: 42.351556,
+		lng: -71.048676
+	},	
+
+]
+
+export default locations;
