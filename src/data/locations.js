67071408403c7d9bc1a26899db356a784dd341ba
Hellen Lee <hleexx@gmail.com>
2018-10-16 15:46:12 -0500

    feat: Add ids to locations

--- src/data/locations.js
+++ src/data/locations.js
@@ -6,21 +6,25 @@ const locations = [
 		lng: -71.056815
 	},
 	{
+		id: "tDPO_D0tjoWUOzqgRUcDkA",
 		name: "Gene's Flatbread",
 		lat: 42.353671,
 		lng: -71.059603
 	},
 	{
+		id: "yBIN7uSzd5saehWA1I_GBw",
 		name: "Row 34",
 		lat: 42.349872,
 		lng: -71.047243
 	},
 	{
+		id: "u51e3gychuqBYNqe2xG6_w",
 		name: "Lolita",
 		lat: 42.350611,
 		lng: -71.051165
 	},
 	{
+		id: "-5gWvrcKOPmhlcZju3tpbw",
 		name: "Flour Bakery",
 		lat: 42.351556,
 		lng: -71.048676
