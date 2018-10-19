ce258de5631fdc0e08600ae80f78291f6f169c88
Hellen Lee <hleexx@gmail.com>
2018-10-18 23:52:22 -0500

    feat: Add info window on marker

--- src/components/Map.js
+++ src/components/Map.js
@@ -10,7 +10,7 @@ class Map extends Component {
 	          		bootstrapURLKeys={{ key: "AIzaSyB_TOMwP-bLmM-Fz4rfd-BhBZTNa4Y6S-c" }}
 	          		center={this.props.center}
 	          		zoom={this.props.zoom}>
-	   				{this.props.locations.map(location => <Place key={location.name} name={location.name} lat={location.lat} lng={location.lng}/>)}
+	   				{this.props.locations.map(location => <Place key={location.name} name={location.name} lat={location.lat} lng={location.lng} image_url={location.image_url}/>)}
 	        	</GoogleMapReact>
 	        </div>
         )
--- src/components/Place.js
+++ src/components/Place.js
@@ -4,6 +4,14 @@ class Place extends Component {
     render() {
         return (
         	<div className ="place">
+        		<div className="info">
+{/*        			<img src={this.props.location.image_url}></img>*/}
+        			<h2>{this.props.name}</h2>
+{/*        			<h3>{locations.categories[0].title}</h3>
+					<h3>{location.location.display_address[0]}<br/>
+					{location.location.display_address[1]}</h3>*/}
+        		</div>
+        			<div className ="place-marker"></div>
 	        </div>
         )
     }
