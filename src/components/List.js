1b4d999fa3e8129f89bcc8cabd52b01f9530486c
Hellen Lee <hleexx@gmail.com>
2018-10-24 13:37:51 -0500

    fix: Accessibility for list items

--- src/components/List.js
+++ src/components/List.js
@@ -16,12 +16,12 @@ class List extends Component {
 				onKeyUp={event => event.keyCode === 13 && this.props.handleSelectedLocation(location.name)}
 				onClick={event => this.props.handleSelectedLocation(location.name)}>
 				<img src={location.image_url} role="img" alt={location.name + " image"}></img>
-				<h2 className="restaurant-name" tabIndex="0">{location.name}</h2>
-				<h3 className="restaurant-categories">{location.categories[0].title}</h3>
-				<h3 className="restaurant address">{location.location.display_address[0]}<br/>
+				<h2 className="restaurant-name" tabIndex="0" aria-labelledby="list item name">{location.name}</h2>
+				<h3 className="restaurant-categories" aria-labelledby="listitem categories">{location.categories[0].title}</h3>
+				<h3 className="restaurant address" aria-labelledby="list item address">{location.location.display_address[0]}<br/>
 				{location.location.display_address[1]}</h3>
-				<h3 className="restaurant phone number">{location.phone}</h3>
-				<h3 className="restaurant availibility">
+				<h3 className="restaurant phone number" aria-labelledby="list item phone number">{location.phone}</h3>
+				<h3 className="restaurant availability" aria-labelledby="list item availability">
 					{this.availabilityText(location.is_open_now)}
 				</h3>
 				<br/>
